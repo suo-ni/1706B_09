db23d6388663e7fe8ab2a044e3c19276f9a3c7a8
Your Name <you@example.com>
2020-04-01 23:30:00 +0800

    完善分类页面

--- 项目代码/easy-market/src/views/main/TopicPage.tsx
+++ 项目代码/easy-market/src/views/main/TopicPage.tsx
@@ -1,8 +1,9 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { topicAction } from '../../store/actions/topic'
 import { RouteComponentProps } from "react-router-dom"
 import style from "../../css/topic.module.scss"
+import BScroll from 'better-scroll';
 import { throttle, debounce } from '../../utils';
 
 interface ItemType {
@@ -13,6 +14,10 @@ interface ItemType {
     scene_pic_url: string
 }
 
+interface BSProps {
+    getBs: (size: number, page: number) => void
+}
+
 interface StateType {
     list: Array<ItemType>
 }
@@ -20,75 +25,96 @@ interface DisptachType {
     getTopicList: () => void
 }
 
-let TopicPage: React.FC<DisptachType & StateType & RouteComponentProps> = (props) => {
+let TopicPage: React.FC<DisptachType & BSProps & StateType & RouteComponentProps> = (props) => {
     // console.log(props.list)
+    let [size, useSize] = useState<number>(10)
     let [page, setPage] = useState<number>(0)
 
- 
+
     useEffect(() => {
         props.getTopicList();
     }, [])
 
-    let getDetail = (e: any) => {
+    let getDetail = (e:any) => {
         let id = e.currentTarget.dataset.id;
-        props.history.push("/topicDetail?id=" + id)
+        // props.history.push("/topicDetail?id=" + id)
     }
 
+    // useEffect(() => {
+    //     props.getBs(size, page)
+    //     setPage(page += 1)
+    //     // console.log(size, page)
+    //     let bs = new BScroll('.tabPageContent', {
+    //         click: true,
+    //         pullUpLoad: {
+    //             threshold: -100
+    //         }
+    //     });
+    //     bs.on('pullingUp', () => {
+    //         props.getBs(size, page)
+    //         setPage(page += 1)
+    //         console.log(size, page)
+    //         bs.finishPullUp()
+    //         bs.refresh()
+    //     })
+    // }, [])
+
     useEffect(()=>{
-        let flag = false;
-        // window.addEventListener('scroll', e => {
-        //     let scrollY = (e.currentTarget as Window).scrollY;
-        //     console.log('scrollY...', scrollY);
-        //     // if (document.documentElement.getBoundingClientRect().height - (window.innerHeight + scrollY) < 20){
-
-        //     // }
-        // })
-        let scrollHandle = (e:Event)=>{
-            if(flag){
-                return;
-            }
-            let scrollY = (e.currentTarget as Window).scrollY
-
-            if(document.documentElement.getBoundingClientReact().height - (window.innerHeight + scrollY) <20){
-                if(props.list.length <= (page+1)*10){
-                    return;
-                }
-                flag = true
-                setPage(page=>page+1)
-                flag = false
-            }
+    let flag = false;
+    let scrollHandle = (e:Event)=>{
+        console.log(e);
+        if(flag){
+            return;
         }
-        let wrapHandle = debounce(scrollHandle);
-        window.addEventListener('scroll',wrapHandle)
-        return () =>{
-            window.removeEventListener("scroll",wrapHandle)
-        }
+        let scrollY = (e.currentTarget as Window).scrollY
+        // if(document.documentElement.offsetHeight - (window.innerHeight+scrollY)<0){
+            // if(props.list.length <= (page+1)*10){
+            //     return;
+            // }
+            // flag = true
+            // setPage(page=>page+1)
+            // flag = false
+        // }
+        
+    }
+    let wrapHandle = debounce(scrollHandle);
+    window.addEventListener('scroll',wrapHandle)
+    return () =>{
+        window.removeEventListener("scroll",wrapHandle)
+    }
     },[props.list])
 
-    return <div className={style.tabPageContent}>
-        {
-            props.list && props.list.slice(0,(page+1)*10).map(item => {
-                return (
-                    <div className={style.topicItem} key={item.id} onClick={getDetail} data-id={item.id}>
-                        <img src={item.scene_pic_url} alt="" />
-                        <div className={style.topicItemTitle}>{item.title}</div>
-                        <div className={style.topicItemSubtitle}>{item.subtitle}</div>
-                        <div className={style.topicItemPrice}>{item.price_info}元起</div>
-                    </div>
-                )
-            })
-        }
-    </div>
+    return <>
+        <div className="tabPageContent">
+            <div className="connect">
+                {
+                    props.list && props.list.map(item => {
+                        return (
+                            <div className="topicItem" key={item.id} onClick={getDetail} data-id={item.id}>
+                                <img src={item.scene_pic_url} alt="" />
+                                <div className="topicItemTitle">{item.title}</div>
+                                <div className="topicItemSubtitle">{item.subtitle}</div>
+                                <div className="topicItemPrice">{item.price_info}元起</div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    </>
 }
 const mapStateToProps = (state: any) => {
     return {
-        list: state.topic.list.data   
+        list: state.topic.list.data
     }
 }
 
 const mapDispatchToProps = (dispatch: Function) => {
     return {
         getTopicList: () => {
+            dispatch(topicAction())
+        },
+        getBs: () => {
             dispatch(topicAction())
         }
     }
--- 项目代码/easy-market/src/views/main/TypePage.tsx
+++ 项目代码/easy-market/src/views/main/TypePage.tsx
@@ -1,30 +1,58 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from "../../css/type.module.scss"
 import { connect } from 'react-redux'
 import { typeAction } from '../../store/actions/type'
 import { RouteComponentProps } from "react-router-dom"
 
-interface ItemType {
+interface category {
     id: number,
     name: string,
     banner_url: string,
     front_name: string
-    icon_url: string
+    icon_url: string,
+    wap_banner_url: string
+
+}
+
+interface currentCategory {
+    id: number,
+    name: string,
+    banner_url: string,
+    front_name: string
+    icon_url: string,
+    wap_banner_url: string,
+    subCategoryList: Array<category>,
+}
+
+interface ItemType {
+    categoryList: Array<category>,
+    currentCategory: currentCategory
 }
 
 interface StateType {
-    typelist: Array<ItemType>
+    typelist: ItemType,
+    tab: Function
 }
 interface DisptachType {
     gettypeList: () => void
 }
 
 let TypePage: React.FC<DisptachType & StateType & RouteComponentProps> = (props) => {
-    console.log(props.typelist)
+    // console.log(props.typelist.currentCategory)
+    let [ind] = useState<number>(1005000)
+    console.log(ind);
     useEffect(() => {
         props.gettypeList();
     }, [])
 
+    let tab = (e: any) => {
+        let id = e.currentTarget.dataset.id;
+        ind = id
+        // setind(id)
+        
+        console.log(ind);
+
+    }
 
     return (
         <div className={style.tabPageContent}>
@@ -33,25 +61,56 @@ let TypePage: React.FC<DisptachType & StateType & RouteComponentProps> = (props)
                     <span>搜索商品，共239款好物</span>
                 </div>
             </div>
+
             <div className={style.tabsWrap} >
                 <div className={style.tab}>
                     {
-                        props.typelist && props.typelist.map(item => (
-                            <div className={style.tabItem} key={item.id}>{item.name}</div>
+                        props.typelist.categoryList && props.typelist.categoryList.map(item => (
+                            <div className={ind === item.id ? 'active' : ''} key={item.id} 
+                                onClick={tab} data-id={item.id}
+                                // changeIndex={(index: number) => {
+                                //     setInd(index);
+                                //   }}
+                                  >
+                                <p className={style.tabItem}>{item.name}</p>
+                            </div>
                         ))
                     }
                 </div>
             </div>
-            <div className={style.categogContet}>
-                <div className={style.categoryWrap}></div>
-                <div className={style.categoryTitle}>居家分类</div>
-            </div>
+            {
+                props.typelist.currentCategory ?
+                    <div className={style.categogContet}>
+                        <div className={style.categoryWrap}>
+                            <span>{props.typelist.currentCategory.front_name}</span>
+                            <img src={props.typelist.currentCategory.wap_banner_url} alt="" />
+                        </div>
+                        <div className={style.categoryTitle}>
+                            <div></div>
+                            居家分类
+                            <div></div>
+                        </div>
+                        <div className={style.subCategory}>
+                            {
+                                props.typelist.currentCategory.subCategoryList.map(v => (
+                                    <div className={style.subCategoryItem} key={v.id}>
+                                        <img src={v.wap_banner_url} alt="" />
+                                        <div className={style.subCategoryItemName}>{v.name}</div>
+                                    </div>
+                                ))
+                            }
+
+                        </div>
+                    </div>
+                    : ''
+            }
+
         </div>
     )
 }
 const mapStateToProps = (state: any) => {
     return {
-        typelist: state.type.typelist.categoryList
+        typelist: state.type.typelist
     }
 }
 
